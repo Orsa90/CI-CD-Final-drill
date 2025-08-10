12fa0cb287264f1729e1939173bb7bf2a9f8930e
Orsa90 <ors294@gmail.com>
2025-08-11 02:12:33 +0300

    add secret - test #3

--- scripts/check_dep_env.sh
+++ scripts/check_dep_env.sh
@@ -2,7 +2,7 @@
 
 echo "🧠 Determining environment..."
 
-if [ "$DEPLOY_ENV" == "DRILL_CI_CD" ]; then
+if [ "$DEPLOY_ENV" == "Thanos" ]; then
   echo "🚀 Deploying to PRODUCTION environment"
 else
   echo "🧪 Deploying to STAGING environment"
